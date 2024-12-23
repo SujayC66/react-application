0192203b4a66761d84d66e25375a128d76bd1270
sujay chogale <sujaychogale@sujays-MacBook-Air.local>
2024-12-23 22:47:11 +0530

    server

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests
 
+from flask import send_from_directory
+
+@app.route('/')
+def serve_react():
+    return send_from_directory('build', 'index.html')
+
 @app.route('/api/data', methods=['GET'])
 def get_data():
     return jsonify({"message": "Hello! My Name is Sujay."})
